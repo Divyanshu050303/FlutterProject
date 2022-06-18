8c639332eff9ba79afae1d65c4739df37056574d
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-06-18 23:23:16 +0530

    homeScreen

---
+++ lib/screen/homeScreen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class homeScreen extends StatelessWidget{
+  const homeScreen({Key?key}):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text("Home Screen"),
+      ),
+      body: Center(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, 'S0');
+            }, child: Text("Screen 0"),
+            ),
+            ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, 'S1');
+            }, child:Text("Screen 1"),
+            ),
+            ElevatedButton(onPressed: (){
+              Navigator.pushNamed(context, 'S2');
+
+            }, child: Text("Screen 2"),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
