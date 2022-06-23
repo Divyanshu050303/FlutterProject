696bbf514d505ebc754f07044be3fc2b73ac124b
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-06-23 20:19:15 +0530

    Menu Demo

--- lib/MenuDemo.dart
+++ lib/MenuDemo.dart
@@ -6,9 +6,9 @@ class MenuDemo extends StatefulWidget{
   State<MenuDemo> createState() => _MenuDemoState();
 }
 class _MenuDemoState extends State<MenuDemo>{
-  var cities=["Delhi", "Mumbai", "Kolkatta", "Chandighar", "Banglore"];
-  var firstCity="Delhi";
-  var choise=["Inbox", "Sent Item", "Junk-mail", "Signout", "Help"];
+  // var cities=["Delhi", "Mumbai", "Kolkatta", "Chandighar", "Banglore"];
+  // var firstCity="Delhi";
+  // var choise=["Inbox", "Sent Item", "Junk-mail", "Signout", "Help"];
   @override
   Widget build(BuildContext context){
     return MaterialApp(
@@ -27,13 +27,13 @@ class _MenuDemoState extends State<MenuDemo>{
               child:const Text("Login",
                 style: TextStyle(color: Colors.white),
               )),
-            PopupMenuButton(onSelected: (String? newItem){
-              print(newItem);
-            },itemBuilder: (BuildContext context){
-              return choise.map((String myChoice){
-                return PopupMenuItem(child: Text(myChoice), value:myChoice);
-              }).toList();
-              })
+            // PopupMenuButton(onSelected: (String? newItem){
+            //   print(newItem);
+            // },itemBuilder: (BuildContext context){
+            //   return choise.map((String myChoice){
+            //     return PopupMenuItem(child: Text(myChoice), value:myChoice);
+            //   }).toList();
+            //   })
           ],
         ),
         body:Center(
@@ -46,23 +46,23 @@ class _MenuDemoState extends State<MenuDemo>{
                   style:TextStyle(fontSize: 30, color:Colors.blue),
                 ),
               ),
-              DropdownButton(items: cities.map((String dropDownItem){
-                return DropdownMenuItem(
-                child:Text(
-                dropDownItem, style:TextStyle(
-              fontSize:20,
-              color:(firstCity==dropDownItem)
-              ?Colors.red:Colors.black),
-                ),
-              value:dropDownItem,
-                );
-              }).toList(),
-              onChanged: (String? newItem){
-              setState((){
-                firstCity=newItem!;
-              });
-              },value:firstCity,
-              )
+              // DropdownButton(items: cities.map((String dropDownItem){
+              //   return DropdownMenuItem(
+              //   child:Text(
+              //   dropDownItem, style:TextStyle(
+              // fontSize:20,
+              // color:(firstCity==dropDownItem)
+              // ?Colors.red:Colors.black),
+              //   ),
+              // value:dropDownItem,
+              //   );
+              // }).toList(),
+              // onChanged: (String? newItem){
+              // setState((){
+              //   firstCity=newItem!;
+              // });
+              // },value:firstCity,
+              // )
             ],
           ),
         ),
